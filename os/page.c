06ec573a8098110fa981c8b51d5b263588ca5807
silence <xiaozhuo1314>
2022-07-24 20:06:18 +0800

    malloc free完成,待测试

--- os/page.c
+++ os/page.c
@@ -85,7 +85,7 @@ struct Block
 
 
 /* 清除页信息,也就是回收页 */
-static inline void _clear(struct Page *page) {
+static inline void _clear_page(struct Page *page) {
     page->flags = 0;
 }
 
@@ -97,19 +97,19 @@ static inline uint32_t _align_page(uint32_t addr)
 }
 
 /* 判断页是否是空闲的 */
-static inline int _is_free(struct Page *page)
+static inline int _is_page_free(struct Page *page)
 {
     return (page->flags & PAGE_TAKEN) ? 0 : 1;
 }
 
 /* 设置flags */
-static inline void _set_flags(struct Page *page, uint8_t flags)
+static inline void _set_page_flags(struct Page *page, uint8_t flags)
 {
     page->flags |= flags;
 }
 
 /* 判断是否是最后一个 */
-static inline int _is_last(struct Page *page)
+static inline int _is_page_last(struct Page *page)
 {
     return (page->flags & PAGE_LAST) ? 1 : 0;
 }
@@ -121,16 +121,24 @@ static inline int _is_malloced(struct Page *page)
 }
 
 /* 通过page获取页内存初始位置 */
-static inline void *_get_mem_by_page(struct Page *page)
+static inline void *_get_start_mem_by_page(struct Page *page)
 {
     return (void*)(_alloc_start + (page - (struct Page*)HEAP_START) * PAGE_SIZE);
 }
 
 /* 通过地址获取页内存的初始位置 */
-static inline void *_get_mem_by_addr(void *p)
+static inline void *_get_start_mem_by_addr(void *p)
 {
     uint32_t page_count = ((uint32_t)p - _alloc_start) / PAGE_SIZE;
-    return (void*)(page_count * PAGE_SIZE + _alloc_start);
+    return (void*)(_alloc_start + page_count * PAGE_SIZE);
+}
+
+/* 通过任意地址获取其所在page  */
+static inline struct Page *_get_page_by_addr(void *p)
+{
+    p = _get_start_mem_by_addr(p);
+    uint32_t page_count = ((uint32_t)p - _alloc_start) / PAGE_SIZE;
+    return ((struct Page*)HEAP_START + page_count);
 }
 
 /* page初始化 */
@@ -146,7 +154,7 @@ void page_init() {
     struct Page *page = (struct Page*)HEAP_START;
     for(int i = 0; i < _num_pages; ++i)
     {
-        _clear(page);
+        _clear_page(page);
         ++page;
     }
 
@@ -178,13 +186,13 @@ void *page_alloc(int npages)
     struct Page *tmp = NULL;
     for(int i = 0; i <= end_page; ++i)
     {
-        if(_is_free(page))
+        if(_is_page_free(page))
         {
             found = 1;
             tmp = page;
             for (int j = 1; j < npages; ++j)
             {
-                if(!_is_free(++tmp))
+                if(!_is_page_free(++tmp))
                 {
                     found = 0;
                     break;
@@ -194,14 +202,14 @@ void *page_alloc(int npages)
             if(found)
             {
                 tmp = page;
-                _set_flags(page, PAGE_FIRST);
+                _set_page_flags(page, PAGE_FIRST);
                 for(int j = 0; j < npages; ++j)
                 {
-                    _set_flags(tmp++, PAGE_TAKEN);
+                    _set_page_flags(tmp++, PAGE_TAKEN);
                 }
                 // 最后一个还需要设置为last
                 --tmp;
-                _set_flags(tmp, PAGE_LAST);
+                _set_page_flags(tmp, PAGE_LAST);
                 return (void*)(_alloc_start + i * PAGE_SIZE);
             }
         }
@@ -217,16 +225,16 @@ void page_free(void *p)
 
     struct Page *page = (struct Page *)HEAP_START + ((uint32_t)p - _alloc_start) / PAGE_SIZE;
 
-    while(!_is_free(page))
+    while(!_is_page_free(page))
     {
-        if(_is_last(page))
+        if(_is_page_last(page))
         {
-            _clear(page);
+            _clear_page(page);
             break;
         }
         else
         {
-            _clear(page++);
+            _clear_page(page++);
         }
     }
 }
@@ -235,9 +243,22 @@ void page_free(void *p)
  * 通过页初始位置获取页中block的初始位置 
  * 参数为页的初始位置
  */
-struct Block *_get_block_by_addr(void *p)
+struct Block *_get_start_block_by_addr(void *p)
 {
     return (struct Block*)(p + ALLOCABLE_SIZE - 1);
+}
+
+/*
+ * 通过任意位置地址获取其所在的block 
+ */
+struct Block *_get_block_by_addr(void *p)
+{
+    // 获取p所在页的起始位置
+    void *page_mem = _get_start_mem_by_addr(p);
+    // 获取p所在页block的起始位置
+    struct Block *block_start = _get_start_block_by_addr(page_mem);
+
+    return block_start + (p - page_mem) / MALLOC_SIZE;
 }
 
 /*
@@ -254,13 +275,19 @@ void _set_block_flags(struct Block *block, uint8_t flags)
     block->flags |= flags;
 }
 
+/* 是否是最后一个block */
+static inline int _is_block_last(struct Block *block)
+{
+    return (block->flags & BLOCK_LAST) ? 1 : 0;
+}
+
 /* 
  * 初始化malloc
  * p指的是页的初始地址 
  */
 struct Block *_malloc_init(void *p)
 {
-    struct Block *block = _get_block_by_addr(p);
+    struct Block *block = _get_start_block_by_addr(p);
     struct Block *tmp = block;
     for (int i = 0; i < MALLOC_TABLE_SIZE; ++i, ++tmp)
     {
@@ -269,7 +296,10 @@ struct Block *_malloc_init(void *p)
     return block;
 }
 
-/* 增加一个页中block使用数目 */
+/* 
+ * 增加一个页中block使用数目 
+ * 参数为页中block管理信息的初始位置
+ */
 static inline void _increase_block_usage_of_malloc(struct Block *block)
 {
     // 页中block数目位的低地址block
@@ -291,19 +321,59 @@ static inline void _increase_block_usage_of_malloc(struct Block *block)
     low_block->flags++; 
 }
 
+/* 
+ * 减少一个页中block使用数目 
+ * 参数为页中block管理信息的初始位置
+ */
+static inline void _decrease_block_usage_of_malloc(struct Block* block)
+{
+    // 页中block数目位的低地址block
+    struct Block *low_block = block + MALLOC_TABLE_SIZE - 1;
+    // 页中block数目位的高地址block
+    struct Block *high_block = block + MALLOC_TABLE_SIZE - 2;
+    // 减一
+    if(low_block->flags == 0)
+    {
+        low_block->flags = 255;
+        high_block->flags -= (1 << 6);
+    }
+    else
+    {
+        low_block->flags -= 1;
+    }
+}
+
+/* 
+ * 获取页中使用的block的数目 
+ * 参数为页中block管理信息的初始位置
+ */
+static inline int _get_memory_usage_of_malloc(struct Block *block)
+{
+    // 页中block数目位的低地址block
+    struct Block *low_block = block + MALLOC_TABLE_SIZE - 1;
+    // 页中block数目位的高地址block
+    struct Block *high_block = block + MALLOC_TABLE_SIZE - 2;
+    // 计算个数
+    int count = high_block->flags;
+    count = (count >> 6) << 8;
+    count += low_block->flags;
+    return count;
+}
+
 /* 尝试去分配内存 */
 void *_try_malloc(void *p, size_t size)
 {
     // 获取页的初始位置
-    p = _get_mem_by_addr(p);
+    p = _get_start_mem_by_addr(p);
     // 获取页中block的初始位置
     struct Block *block, *block_start;
-    block_start = block = _get_block_by_addr(p);
+    block_start = block = _get_start_block_by_addr(p);
     // 需要多少个block
     int block_total = size / MALLOC_SIZE;
     if(size % MALLOC_SIZE) block_total += 1;
+    if(block_total == 0) return NULL;
     int end_index = MALLOC_TABLE_SIZE - block_total;
-    for(int i = 0; i < end_index; ++i, ++block) // 由于MALLOC_TABLE_SIZE比实际的block个数多1,所以这里不加等于
+    for(int i = 0; i < end_index; ++i, ++block) // 由于MALLOC_TABLE_SIZE比实际的block个数多1,而且820那个是代表的block个数,不允许分配出去,所以这里不加等于
     {
         if(_is_block_free(block))
         {
@@ -345,6 +415,7 @@ void *_try_malloc(void *p, size_t size)
  */
 void *malloc(size_t size)
 {
+    if(size <= 0) return NULL;
     void *mem = NULL;
     /* 判断待分配内存的大小 */
     if(size <= ALLOCABLE_SIZE) // 按照malloc来分配
@@ -355,14 +426,17 @@ void *malloc(size_t size)
             // 当前页是被malloc控制的,那么就尝试在这个里面分配内存
             if(_is_malloced(page))
             {
-                
+                // 获取页内存初始位置
+                mem = _get_start_mem_by_page(page);
+                // 尝试去分配block内存,成功则返回,否则就进行下一循环
+                if((mem = _try_malloc(mem, size)) != NULL) return mem;
             }
-            else if(_is_free(page)) // 如果不是被malloc控制但是空闲
+            else if(_is_page_free(page)) // 如果不是被malloc控制但是空闲
             {
                 // 设置页的属性
-                _set_flags(page, PAGE_TAKEN | PAGE_LAST | PAGE_FIRST | PAGE_MALLOC);
+                _set_page_flags(page, PAGE_TAKEN | PAGE_LAST | PAGE_FIRST | PAGE_MALLOC);
                 // 获取页内存初始位置
-                mem = _get_mem_by_page(page);
+                mem = _get_start_mem_by_page(page);
                 // 初始化malloc管理信息
                 _malloc_init(mem);
                 // 尝试去分配block内存,成功则返回,否则就进行下一循环
@@ -372,25 +446,352 @@ void *malloc(size_t size)
     }
     else if(size > ALLOCABLE_SIZE && size <= PAGE_SIZE) // 直接按照一页来分配
     {
-
+        struct Page *page = (struct Page*)HEAP_START;
+        for(int i = 0; i < _num_pages; ++i, ++page)
+        {
+            if(_is_page_free(page) && (mem = page_alloc(1))) return mem;
+        }
     }
-    else // 前面的按照页,剩下的按照malloc分配
+    else // 前面的按照页,剩下的按照malloc分配,而且剩下的不足malloc的部分要跟前面分配的整页连续
     {
-
+        int page_count = size / PAGE_SIZE;
+        int rest = size % PAGE_SIZE;
+        if(rest) page_count += 1;
+        mem = page_alloc(page_count);
+        if(rest > 0 && rest <= ALLOCABLE_SIZE && mem)
+        {
+            void *p = mem + (page_count - 1) * PAGE_SIZE;
+            _malloc_init(p);
+            struct Page *page = _get_page_by_addr(p);
+            _set_page_flags(page, PAGE_MALLOC);
+            _try_malloc(p, rest);
+        }
     }
     return mem;
 }
 
+void free(void *p)
+{
+    if(p == NULL || (uint32_t)p < _alloc_start || (uint32_t)p > _alloc_end)
+    {
+        panic("Memory panic: Can't free this memory");
+         return;
+    }
+    // 获取p所在的页
+    struct Page *page = _get_page_by_addr(p);
+    /* 
+     * 对于malloc分配的三种大情况
+     * 第一种清空页中block信息等
+     * 第二种直接page_free即可
+     * 第三种分为两种小情况:
+     *   如果最后一页不是malloc管理的,直接page_free即可
+     *   如果最后一页是malloc管理的,需要按照上面的第一种大情况来处理
+     * 基于以上分析,由于第三种大情况可能会用到第一种大情况,所以要按照以下步骤
+     * 1.应该先去判断第三种大情况,即p所在的初始page不是malloc管理的,然后清除不是malloc管理的若干页信息
+     *   1.1最后一页若也不是malloc管理的,由于page_free会将所有page清除,那么就完成了第三种大情况的第一种小情况
+     *   1.2最后一个若是malloc管理的,就需要在倒数第二页加上一个PAGE_LAST标志(这样page_free才不会直接把最后malloc管理的页清除,因为其中有可能有属于其他内存区域的内存),然后去3中清除
+     * 2.第二种大情况在1.1中就会被包含了
+     * 3.第三种大情况清除malloc管理的内存
+     */
+    if(!_is_malloced(page))
+    {
+        // 判断最后一页是不是malloc管理的
+        while(!_is_page_last(page)) ++page;
+        if(!_is_malloced(page)) //最后一页不是malloc控制的
+        {
+            page_free(p);
+            return;
+        }
+        else
+        {
+            // 设置倒数第二页为last,这样page_free就只能清除到倒数第二页
+            _set_page_flags(page - 1, PAGE_LAST);
+            page_free(p);
+            // 让p指向最后一页malloc的初始位置,因为最后malloc控制的页中的block内存必然与前面的整页内存是连续的
+            // 所以最后malloc控制的页的block初始位置也就是该页的初始位置
+            p = _get_start_mem_by_addr(p);
+        }
+    }
+    if(_is_malloced(page))
+    {
+        // 获取管理p的block
+        struct Block *block = _get_block_by_addr(p);
+        // 获取p所在页的block管理信息的初始block
+        struct Block *block_start = _get_start_block_by_addr(_get_start_mem_by_addr(p));
+        while(!_is_block_free(block))
+        {
+            /*
+             * 最后一个block不可能是820那个
+             * 因为820那个没有管理任何block
+             * 而这个限制在malloc的_try_malloc的end_index就已经限制了
+             * 同时819那个最高两位是个数而不是属性,所以不能直接等于0
+             * 其他的block管理信息的最高两位没啥用,本来就是0
+             * 所以这里清除block信息直接低6位置0即可
+             */
+            block->flags = (block->flags >> 6) << 6;
+            // 减少block数量
+            _decrease_block_usage_of_malloc(block_start);
+            if(_is_block_last(block))
+            {
+                break;
+            }
+            ++block;
+        }
+        // 如果已经没有block在使用了,就需要清除页的PAGE_MALLOC标志,且释放掉该页内存
+        if(_get_memory_usage_of_malloc(block_start) == 0)
+        {
+            page->flags ^= PAGE_MALLOC;
+            page_free(_get_start_mem_by_page(page));
+        }
+    }
+}
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    if((uint32_t)dest < _alloc_start || ((uint32_t)dest + n) > _alloc_end)
+    {
+        panic("Memcpy Panic: destination oversteps the boundary");
+        return NULL;
+    }
+    if(dest == src)
+        return dest;
+
+    char *dp = dest;
+    const char *sp = src;
+    if(dest < src)
+    {
+        while(n--)
+            *(dp++) = *(sp++);
+    }
+    else
+    {
+        dp += n;
+        sp += n;
+        while(n--)
+            *(--dp) = *(--sp);
+    }
+    return dest;
+}
+
+void printPageInfo(struct Page * page, int pageNumToDisplay){
+    int counter = 1;
+    for(int i = 0;i < pageNumToDisplay; i++,counter++){
+        printf("| %d ", (page+i)->flags);
+
+        // if(counter == 4)
+        //     printf("|=");
+        if(counter == 8)
+            printf("\n"); 
+        counter%=8;
+    }
+    printf("\n");
+}
+
+void printBlockInfo(struct Block * block, int blockNumToDisplay){
+    int counter = 1;
+    for(int i = 0;i < blockNumToDisplay; i++,counter++){
+        printf("| %d ", (block+i)->flags);
+
+        // if(counter == 4)
+        //     printf("|=");
+        if(counter == 8)
+            printf("\n"); 
+        counter%=8;
+    }
+    printf("\n");
+}
+
 void page_test()
 {
-    void *p = page_alloc(1);
+    struct Page *page = (struct Page *)HEAP_START;
+    printf("\n\n==============> page_test <==============\n\n");
+
+    printPageInfo(page,16);
+
+	printf("void *p = page_alloc(2);\n");
+	void *p = page_alloc(2);
 	printf("p = 0x%x\n", p);
-	page_free(p);
+	//page_free(p);
 
-	// void *p2 = page_alloc(7);
-	// printf("p2 = 0x%x\n", p2);
-	// page_free(p2);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
 
-	// void *p3 = page_alloc(4);
-	// printf("p3 = 0x%x\n", p3);
+	printf("void *p2 = page_alloc(7);\n");
+    void *p2 = page_alloc(7);
+	printf("p2 = 0x%x\n", p2);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("page_free(p2);\n");
+	page_free(p2);
+
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+	printf("void *p3 = page_alloc(4);\n");
+	void *p3 = page_alloc(4);
+	printf("p3 = 0x%x\n", p3);
+
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("page_free(p+4096);\n");
+    page_free(p+4096);
+
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("void *p4 = page_alloc(4);\n");
+	void *p4 = page_alloc(4);
+	printf("p4 = 0x%x\n", p4);
+
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("void *p5 = page_alloc(1);\n");
+	void *p5 = page_alloc(1);
+	printf("p5 = 0x%x\n", p5);
+
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("page_free(p);\n");
+    page_free(p);
+
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("void *p6 = page_alloc(1);\n");
+	void *p6 = page_alloc(1);
+	printf("p5 = 0x%x\n", p6);
+
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("\n\n==============> END page_test <==============\n\n");
+}
+
+void molloc_test()
+{
+    printf("\n\n==============> molloc_test <==============\n\n");
+    
+    page_init();
+    
+    printf("most operation are in first Pages\n");
+
+    struct Page *page = (struct Page *)HEAP_START;
+    void * page1Mem = (void*)_alloc_start;
+    struct Block * mallocTable = _get_start_block_by_addr(page1Mem);
+    printf("page1 = 0x%x\n", page1Mem);
+    printf("Page Table:\n");
+    printBlockInfo(mallocTable, 8);
+
+    printf("void * page1 = page_alloc(1);\n");
+    void * page1 = page_alloc(1);
+    printf("page1 = 0x%x\n", page1);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("void * page2 = page_alloc(1);\n");
+    void * page2 = page_alloc(1);
+    printf("page2 = 0x%x\n", page2);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("free(page1);\n");
+	free(page1);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+    
+    printf("void * p1 = malloc(sizeof(char));\n");
+    void * p1 = malloc(sizeof(char));
+    printf("p1 = 0x%x\n", p1);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("free(p1);\n");
+    free(p1);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("void * p2 = malloc(sizeof(short));\n");
+    void * p2 = malloc(sizeof(short));
+    printf("p2 = 0x%x\n", p2);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("void * p3 = malloc(sizeof(int));\n");
+    void * p3 = malloc(sizeof(int));
+    printf("p3 = 0x%x\n", p3);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("void * p4 = malloc(sizeof(long long int));\n");
+    void * p4 = malloc(sizeof(long long int));
+    printf("p4 = 0x%x\n", p4);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("void * p5 = malloc(sizeof(long double));\n");
+    void * p5 = malloc(sizeof(long double));
+    printf("p5 = 0x%x\n", p5);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("free(p5+4);\n");
+    free(p5+4);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("free(p4+4);\n");
+    free(p4+4);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("void * p6 = malloc(sizeof(int));\n");
+    void * p6 = malloc(sizeof(int));
+    printf("p6 = 0x%x\n", p6);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("void * p7 = malloc(sizeof(int)*819);\n");
+    void * p7 = malloc(sizeof(int)*819);
+    printf("p7 = 0x%x\n", p7);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    free(p7+sizeof(int)*818);
+    printf("free(p7+sizeof(int)*818);\n");
+    void *tmp = (void *)(_alloc_start + (PAGE_SIZE * 2));
+    printBlockInfo(_get_start_block_by_addr(tmp)+800, 20);
+    printf("memory usage ==> %d\n", _get_memory_usage_of_malloc(_get_start_block_by_addr(tmp)));
+ 
+    
+    printf("void * p8 = malloc(sizeof(int)*4000);\n");
+    void * p8 = malloc(sizeof(int)*4000);
+    printf("p8 = 0x%x\n", p8);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("free(p8+4096);\n");
+    page_free(p8+4096);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("free(p8);\n");
+    page_free(p8);
+    printf("Page Table:\n");
+    printPageInfo(page,16);
+
+    printf("void * p9 = malloc(sizeof(int));\n");
+    void * p9 = malloc(sizeof(int));
+    printf("p9 = 0x%x\n", p9);
+    printf("Molloc Table:\n");
+    printBlockInfo(mallocTable, 40);
+
+    printf("\n\n==============> END molloc_test <==============\n\n"); 
 }
